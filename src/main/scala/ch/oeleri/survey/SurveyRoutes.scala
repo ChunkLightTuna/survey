90e729fa00931771d2d293213929d70acf2f6af1
Chris Oelerich <coelerich@dv01.co>
2020-04-13 14:03:29 -0400

    delay execution

--- src/main/scala/ch/oeleri/survey/SurveyRoutes.scala
+++ src/main/scala/ch/oeleri/survey/SurveyRoutes.scala
@@ -1,37 +1,24 @@
 package ch.oeleri.survey
 
 import java.io.{File, FileOutputStream}
-import java.nio.charset.StandardCharsets
+import java.util.concurrent._
 
 import cats.effect.{Blocker, ContextShift, IO}
-import org.http4s.{HttpRoutes, StaticFile}
+import cats.implicits._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.{HttpRoutes, StaticFile}
 
-trait Question extends Product {
-  val question: String
-}
-
-case class Text(question: String, response: String) extends Question {
-  override def toString: String = s"$question:\n$response"
-}
-
-case class Audio(question: String, response: Array[Byte]) extends Question
+import scala.concurrent.ExecutionContext
 
 object SurveyRoutes {
 
   def surveyRoutes(): HttpRoutes[IO] = {
     val dsl = new Http4sDsl[IO] {}
-
-    import java.util.concurrent._
+    import dsl._
 
     val blockingPool = Executors.newFixedThreadPool(4)
     val blocker = Blocker.liftExecutorService(blockingPool)
-
-    import dsl._
-    import scala.concurrent.ExecutionContext
-
     implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-
 
     HttpRoutes.of[IO] {
       case request@GET -> "static" /: x =>
@@ -49,27 +36,25 @@ object SurveyRoutes {
         val path = s"${System.getProperty("user.home")}/responses/${millis}_$ip"
 
         req.decodeWith(org.http4s.EntityDecoder.multipart[IO], strict = true) { response =>
-          val parts: Seq[Question] = response.parts.withFilter(_.name.isDefined).map {
-            case mp3 if mp3.filename.isDefined =>
-              Audio(mp3.name.get, mp3.body.compile.to(Array).unsafeRunSync())
-            case text =>
-              Text(text.name.get, text.body.compile.to(Array).map(bytes => new String(bytes, StandardCharsets.UTF_8)).unsafeRunSync())
+          val (text, audio) = response.parts.partition(_.filename.isEmpty)
+
+          val writeText = text.map { part =>
+            part.body.through(fs2.text.utf8Decode).compile.string
+              .map(answer => s"${part.name.get}:\t$answer")
+          }.sequence.map { questions =>
+            new FileOutputStream(s"$path/questions.txt")
+              .write(questions.mkString("\n\n").getBytes)
           }
 
-          val text = parts.withFilter(_.isInstanceOf[Text]).map(_.asInstanceOf[Text])
-          val audio = parts.withFilter(_.isInstanceOf[Audio]).map(_.asInstanceOf[Audio])
-
+          val writeAudio = audio.map { part =>
+            part.body.compile.to(Array).map { bytes =>
+              new FileOutputStream(s"$path/${part.filename}").write(bytes)
+            }
+          }.sequence
 
           new File(path).mkdir()
-
-          new FileOutputStream(s"$path/questions.txt")
-            .write(text.mkString("", "\n\n", "\n").getBytes)
-
-          audio.foreach { mp3 =>
-            new FileOutputStream(s"$path/${mp3.question}.mp3")
-              .write(mp3.response)
-          }
-
+          writeText.unsafeRunSync()
+          writeAudio.unsafeRunSync()
 
           Ok("success")
         }
